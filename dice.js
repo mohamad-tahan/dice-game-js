764a15fef5aceac7b4c462bb0c4d814c23e52fe6
Moe Tahan <mohamadtahan880@gmail.com>
2022-05-23 11:15:21 +0300

    JS: displaying images

--- dice.js
+++ dice.js
@@ -3,3 +3,6 @@ var randomImage1 = "images/dice" + randomDice1 + ".png";
 
 var randomDice2 = Math.floor(Math.random() * 6) + 1;
 var randomImage2 = "images/dice" + randomDice2 + ".png";
+
+var image1 = document.querySelector(".img1").setAttribute("src", randomImage1);
+var image1 = document.querySelector(".img2").setAttribute("src", randomImage2);
