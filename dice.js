102847b8df78603dbafa47cbe6b286497e7322c5
Moe Tahan <mohamadtahan880@gmail.com>
2022-05-23 11:13:36 +0300

    JS: Generating random number and image

---
+++ dice.js
@@ -0,0 +1,5 @@
+var randomDice1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "images/dice" + randomDice1 + ".png";
+
+var randomDice2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomDice2 + ".png";
